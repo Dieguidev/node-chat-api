cf46d4a0dad0cf43df975ce503943cd84a31a83b
Dieguidev <diegogaraycullas@gmail.com>
2023-09-20 21:45:37 -0500

    corregidos errores de relacion en init.models

--- src/db/model/init-models.js
+++ src/db/model/init-models.js
@@ -7,16 +7,46 @@ const Users = require('./users.model');
 const initModels = () => {
   Users.hasMany(Messages, { as: 'messages', foreignKey: 'senderUserId' });
   Messages.belongsTo(Users, { as: 'senderUser', foreignKey: 'senderUserId' });
-  Users.hasMany(Conversations, { as: 'conversations', foreignKey: 'createdByUserId' });
-  Conversations.belongsTo(Users, { as: 'createdByUser', foreignKey: 'createdByUserId' });
-  Conversations.hasMany(Messages, { as: 'messages', foreignKey: 'conversationId' });
-  Messages.belongsTo(Conversations, { as: 'conversation', foreignKey: 'conversationId' });
-  Conversations.hasMany(Participants, { as: 'participants', foreignKey: 'conversationId' });
-  Participants.belongsTo(Conversations, { as: 'conversation', foreignKey: 'conversationId' });
-  Users.hasMany(Participants, { as: 'participants', foreignKey: 'userId' });
-  Participants.belongsTo(Users, {as: 'user', foreignKey: 'userId'});
-  ConversationTypes.hasMany(Conversations, { as: 'conversations', foreignKey: 'conversationTypeId' });
-  Conversations.belongsTo(ConversationTypes, { as: 'conversationType', foreignKey: 'conversationTypeId' });
-}
+  Users.hasMany(Conversations, {
+    as: 'conversations',
+    foreignKey: 'createdByUserId',
+  });
+  Conversations.belongsTo(Users, {
+    as: 'createdByUser',
+    foreignKey: 'createdByUserId',
+  });
+  Conversations.hasMany(Messages, {
+    as: 'messages',
+    foreignKey: 'conversationId',
+  });
+  Messages.belongsTo(Conversations, {
+    as: 'conversation',
+    foreignKey: 'conversationId',
+  });
+  Conversations.hasMany(Participants, {
+    as: 'participants',
+    foreignKey: 'conversationId',
+  });
+  Participants.belongsTo(Conversations, {
+    as: 'conversation',
+    foreignKey: 'conversationId',
+  });
+  Users.hasMany(Participants, {
+    as: 'participants',
+    foreignKey: 'participantUserId',
+  });
+  Participants.belongsTo(Users, {
+    as: 'user',
+    foreignKey: 'participantUserId',
+  });
+  ConversationTypes.hasMany(Conversations, {
+    as: 'conversations',
+    foreignKey: 'typeConversationId',
+  });
+  Conversations.belongsTo(ConversationTypes, {
+    as: 'conversationType',
+    foreignKey: 'typeConversationId',
+  });
+};
 
 module.exports = initModels;
